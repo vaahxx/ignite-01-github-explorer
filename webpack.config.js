9aa62b978b74af20394c9b6747e7b1781a2d3df8
Valentina Garavaglia <vaahxx@gmail.com>
2022-10-09 18:44:11 -0400

    using typescript

--- webpack.config.js
+++ webpack.config.js
@@ -6,18 +6,18 @@ const isDevelopment = process.env.NODE_ENV !== "production";
 module.exports = {
   mode: isDevelopment ? "development" : "production",
   devtool: isDevelopment ? "eval-source-map" : "source-map",
-  entry: path.resolve(__dirname, "src", "index.jsx"),
+  entry: path.resolve(__dirname, "src", "index.tsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: [".js", ".jsx", "ts", "tsx"],
   },
   module: {
     rules: [
       {
-        test: /\.jsx$/,
+        test: /\.(j|t)sx$/,
         exclude: /node_modules/,
         use: "babel-loader",
       },
