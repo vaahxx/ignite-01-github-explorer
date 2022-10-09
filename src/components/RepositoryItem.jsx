893fc5c3bd8ce1ec7bb82dd5c670e2514897462b
Valentina Garavaglia <vaahxx@gmail.com>
2022-10-09 17:04:40 -0400

    list repositories functionality

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -3,7 +3,7 @@ export function RepositoryItem(props) {
     <li>
       <strong>{props.repository.name}</strong>
       <p>{props.repository.description}</p>
-      <a href={props.repository.link}>Access repository</a>
+      <a href={props.repository.html_url}>Access repository</a>
     </li>
   );
 }
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,20 +1,22 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 import "../styles/repositories.scss";
 
-const repository = {
-  name: "unform",
-  description: "description",
-  link: "https://github.com/vaahxx",
-};
-
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+  useEffect(() => {
+    fetch("https://api.github.com/users/vaahxx/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Repository List</h1>
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map((repository) => (
+          <RepositoryItem key={repository.name} repository={repository} />
+        ))}
       </ul>
     </section>
   );
